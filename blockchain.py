ded0ae5dbbd07ae7d53a8ca3e7785b493d6c4f85
Liu.Peixin <liupeixin1989@gmail.com>
2018-01-21 21:32:22 -0800

    update

--- blockchain.py
+++ blockchain.py
@@ -1,24 +1,70 @@
-
+import hashlib
+import json
+from time import time
 
 class Blockchain(object):
     def __init__(self):
-        self.chain = []
         self.current_transactions = []
+        self.chain = []
+
+        # 创建创世区块
+        self.new_block(previous_hash=1, proof=100)
+
 
     def new_block(self):
-        # Creates a new Block and adds it to the chain
-        pass
 
-    def new_transaction(self):
-        # Adds a new transaction to the list of transactions
-        pass
+		"""
+        创建一个新的区块到区块链中
+        :param proof: <int> 由工作证明算法生成的证明
+        :param previous_hash: (Optional) <str> 前一个区块的 hash 值
+        :return: <dict> 新区块
+        """
+
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+
+        # 重置当前交易记录
+        self.current_transactions = []
+
+        self.chain.append(block)
+        return block
+
+
+def new_transaction(self, sender, recipient, amount):
+        """
+        创建一笔新的交易到下一个被挖掘的区块中
+        :param sender: <str> 发送人的地址
+        :param recipient: <str> 接收人的地址
+        :param amount: <int> 金额
+        :return: <int> 持有本次交易的区块索引
+        """
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.last_block['index'] + 1
+
 
     @staticmethod
     def hash(block):
-        # Hashes a Block
-        pass
+        """
+        给一个区块生成 SHA-256 值
+        :param block: <dict> Block
+        :return: <str>
+        """
+
+        # 我们必须确保这个字典（区块）是经过排序的，否则我们将会得到不一致的散列
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
+
 
     @property
     def last_block(self):
-        # Returns the last Block in the chain
-        pass
+        return self.chain[-1]
